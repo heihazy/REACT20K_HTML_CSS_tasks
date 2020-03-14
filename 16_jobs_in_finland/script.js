a4bd6355a246af748a2a89c65a49648d3bd5cf25
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-03-14 12:42:28 +0200

    finished styling region section and footer

--- 16_jobs_in_finland/script.js
+++ 16_jobs_in_finland/script.js
@@ -4,3 +4,19 @@ $("document").ready(function() {
     $("input[type='text']").focus();
   });
 });
+
+var myBtn = document.getElementById("back-to-top");
+window.onscroll = function() {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
