d55ec0e19430b149d5ca2871b380531dcc967285
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-03-17 13:24:54 +0200

    finished back to top button, sticky navbar and updated css

--- 16_jobs_in_finland/script.js
+++ 16_jobs_in_finland/script.js
@@ -1,22 +1,39 @@
 $("document").ready(function() {
+  // display search area
   $(".fa-search").click(function() {
     $(".toggle-search").toggle();
     $("input[type='text']").focus();
   });
-});
 
-var myBtn = document.getElementById("back-to-top");
-window.onscroll = function() {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myBtn.style.display = "block";
-  } else {
-    myBtn.style.display = "none";
+  // sticky navbar
+  var navbar = document.getElementById("menu");
+  var sticky = navbar.offsetTop;
+  function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+
+  // back to top button
+  var myBtn = document.getElementById("back-to-top");
+  window.onscroll = function() {
+    stickyNav();
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      myBtn.style.display = "block";
+    } else {
+      myBtn.style.display = "none";
+    }
+  }
+  $("#back-to-top").click(function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+});
